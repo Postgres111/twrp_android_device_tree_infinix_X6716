2bce99c0d8add69c17ecef7ba3e28e01d01bce8a
Gilang EA. <144137514+excaliburXD@users.noreply.github.com>
2025-06-29 20:37:03 +0700

    Infinix-X694: Setup FBEv2

--- twrp_X693.mk
+++ twrp_X693.mk
@@ -10,6 +10,13 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit from X693 device
 $(call inherit-product, device/infinix/X693/device.mk)
 
+# Enable proper FBE v2 decryption for Android 11+ (especially for GSI)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.crypto.dm_default_key.options_format.version=2 \
+    ro.crypto.volume.filenames_mode=aes-256-cts \
+    ro.crypto.volume.metadata.method=dm-default-key \
+    ro.crypto.volume.options=::v2
+
 # Product Specifics
 PRODUCT_NAME := twrp_X693
 PRODUCT_DEVICE := X693
